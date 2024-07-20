4a017a1b73078c512b075eff4f7e0dea2db4a62a
mufaisa <mfkamalimdia@gmail.com>
2024-07-21 03:35:35 +0530

    changes in ConnecDB file

--- ConnectDB/ConnectDB.js
+++ ConnectDB/ConnectDB.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
+const userModel = require('../Models/usermodel.js')
 
 const ConnectDB = async () => {
     try {
         await mongoose.connect(`mongodb+srv://mfk:test123@portfolio.lsuj3bc.mongodb.net/Blogging_App?retryWrites=true&w=majority&appName=Portfolio`)
 
+        const users = await userModel.find()
+
         console.log(`DB Connected`)
+        
+        console.log(users[0].Name)
 
     } catch (error) {
         console.log(error);
