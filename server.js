4b2b1c8e54122348474c63aee6c0088a1f7ec53a
mufaisa <mfkamalimdia@gmail.com>
2024-07-27 16:27:34 +0530

    changes

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const { userRouter } = require('./Routes/userRouter.js')
 const ConnectDB = require('./ConnectDB/ConnectDB.js')
+const { CorsOptions } = require('./CorsOptions/CorsOptions.js')
 
 
 const app = express()
 
 app.use(express.json())
 
-app.use(cors({
-    origin: [],
-    methods: ['GET', 'POST'],
-    credentials: true
-}))
+app.use(cors(CorsOptions))
 
 app.use(cookieParser())
 
