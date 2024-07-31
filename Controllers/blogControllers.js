91bc2222580a8b8902e577b672e2277c6c32eee5
mufaisa <mfkamalimdia@gmail.com>
2024-07-31 18:45:05 +0530

    Like/Unlike route and controller added.

--- Controllers/blogControllers.js
+++ Controllers/blogControllers.js
@@ -9,3 +9,33 @@ exports.CreateBlog = (req, res) => {
         .catch(er => console.log(er))
 
 }
+
+exports.LikeUnlikeBlog = async (req, res) => {
+    const { blogid } = req.params
+
+    try {
+        const targetblog = await blogModel.findById({ _id: blogid })
+
+        if (targetblog.Likes.includes(req.user._id)) {
+
+            const index = targetblog.Likes.indexOf(req.user._id)
+            targetblog.Likes.splice(index, 1)
+
+            await targetblog.save()
+
+            res.json("Unliked")
+
+        }
+
+        else {
+            targetblog.Likes.push(req.user._id)
+            await targetblog.save()
+            res.json("Liked")
+        }
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
--- Routes/blogRouter.js
+++ Routes/blogRouter.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const VerifyToken = require("../VerifyToken/VerifyToken")
-const { CreateBlog } = require("../Controllers/blogControllers.js")
+const { CreateBlog, LikeUnlikeBlog } = require("../Controllers/blogControllers.js")
 
 const blogRouter = express.Router()
 
 blogRouter.post('/createblog', VerifyToken, CreateBlog)
+
+blogRouter.patch('/likeunlikeblog/:blogid', VerifyToken, LikeUnlikeBlog)
 
 module.exports = blogRouter
