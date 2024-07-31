acb1b14543946611c2427c863f7c320d0b816459
mufaisa <mfkamalimdia@gmail.com>
2024-07-31 22:31:22 +0530

    EditBlogText route and controller added.

--- Routes/blogRouter.js
+++ Routes/blogRouter.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const VerifyToken = require("../VerifyToken/VerifyToken")
-const { CreateBlog, LikeUnlikeBlog, AddComment, DeleteComment, EditComment } = require("../Controllers/blogControllers.js")
+const { CreateBlog, LikeUnlikeBlog, AddComment, DeleteComment, EditComment, EditBlogText } = require("../Controllers/blogControllers.js")
 
 const blogRouter = express.Router()
 
@@ -13,5 +13,7 @@ blogRouter.post('/addcomment/:blogid', VerifyToken, AddComment)
 blogRouter.patch('/deletecomment/:blogid/:commentid', VerifyToken, DeleteComment)
 
 blogRouter.patch('/editcomment/:blogid/:commentid', VerifyToken, EditComment)
+
+blogRouter.patch('/editblogtext/:blogid', VerifyToken, EditBlogText)
 
 module.exports = blogRouter
