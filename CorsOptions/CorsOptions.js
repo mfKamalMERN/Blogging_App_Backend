595bcb8419bf28d992e234c1de220c698d9a124b
mufaisa <mfkamalimdia@gmail.com>
2024-08-18 02:10:43 +0530

    added frontend url in cors options.

--- CorsOptions/CorsOptions.js
+++ CorsOptions/CorsOptions.js
@@ -2,7 +2,7 @@
 
 exports.CorsOptions = {
 
-    origin: [`http://localhost:3000`, `http://localhost:3000/home`, `http://localhost:3000/followings/:userid`, `http://localhost:3000/followers/:userid`, `http://localhost:3000/profile/:userid`, `http://localhost:3000/newblog`],
+    origin: [`http://localhost:3000`, `http://localhost:3000/home`, `http://localhost:3000/followings/:userid`, `http://localhost:3000/followers/:userid`, `http://localhost:3000/profile/:userid`, `http://localhost:3000/newblog`, `https://blogging-app-frontend-seven.vercel.app`, `https://blogging-app-frontend-seven.vercel.app/home`, `https://blogging-app-frontend-seven.vercel.app/followings/:userid`, `https://blogging-app-frontend-seven.vercel.app/profile/:userid`, `https://blogging-app-frontend-seven.vercel.app/newblog`],
 
     methods: ['GET', 'POST', 'PUT', 'DELETE', `PATCH`],
 
