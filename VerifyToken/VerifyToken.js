50a9f5027c381145da40bd8edbc9cdacddaa0438
mufaisa <mfkamalimdia@gmail.com>
2024-08-24 02:11:19 +0530

    c

--- VerifyToken/VerifyToken.js
+++ VerifyToken/VerifyToken.js
@@ -6,19 +6,18 @@ const VerifyToken = (req, res, next) => {
 
     const token = req.cookies.token
 
-    if (!token) {
-        res.json(`Missing token`)
-    }
+    if (!token) res.json(`Missing token`)
 
     else {
         jwt.verify(token, "jwt-secret-key", async (err, decoded) => {
+
             if (err) res.json(err)
 
             else {
                 try {
                     req.user = await userModel.findById({ _id: decoded._id })
-                    next()
 
+                    next()
                 } catch (error) {
                     console.log(error)
                 }
