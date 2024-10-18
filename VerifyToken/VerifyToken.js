b64b6aec1f06b87068cc0e77674a8023f168a11f
mufaisa <mfkamalimdia@gmail.com>
2024-10-19 04:45:15 +0530

    VerifyToken modified.

--- VerifyToken/VerifyToken.js
+++ VerifyToken/VerifyToken.js
@@ -9,27 +9,27 @@ const VerifyToken = (req, res, next) => {
     if (!token) res.json(`Missing token`)
 
     else {
-        jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+        jwt.verify(token, "jwt-secret-key", async (err, decoded) => {
 
             if (err) res.json(err)
 
-            // else {
-            //     try {
-            //         req.user = await userModel.findById({ _id: decoded._id })
-
-            //         next()
-            //     } catch (error) {
-            //         console.log(error)
-            //     }
-            // }
             else {
-                userModel.findById({ _id: decoded._id })
-                    .then((usr) => {
-                        req.user = usr;
-                        next();
-                    })
-                    .catch(er => console.log(er))
+                try {
+                    req.user = await userModel.findById({ _id: decoded._id })
+
+                    next()
+                } catch (error) {
+                    console.log(error)
+                }
             }
+            // else {
+            //     userModel.findById({ _id: decoded._id })
+            //         .then((usr) => {
+            //             req.user = usr;
+            //             next();
+            //         })
+            //         .catch(er => console.log(er))
+            // }
         })
     }
 
