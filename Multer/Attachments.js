8d00ea64c08f606215f3fb08ae35d63f3d7c164e
mufaisa <mfkamalimdia@gmail.com>
2024-11-18 08:27:12 +0530

    c

--- Multer/Attachments.js
+++ Multer/Attachments.js
@@ -1,12 +1,12 @@
 const multer = require("multer");
 
 
-const storage = multer.diskStorage({
+const storagee = multer.diskStorage({
     destination: (req, file, cb) => cb(null, 'Public/Attachments'),
 
     filename: (req, file, cb) => cb(null, Date.now() + file.originalname)
 })
 
-const uploadAttachment = multer({ storage });
+const uploadAttachment = multer({ storage: storagee });
 
 module.exports = uploadAttachment;
